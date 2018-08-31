1808a1f86cbf013aeef96158ea66e23809530c65
Benjamin Congdon <bencongdon96@gmail.com>
2018-08-30 21:15:50 -0700

    Add ability to switch between input field and table

--- main.go
+++ main.go
@@ -42,12 +42,43 @@ func getEmojis() (map[string][]Emoji, error) {
 	return keywordMap, nil
 }
 
+func drawEmojis(table *tview.Table, emojis []string) {
+	cols, rows := 10, 200
+	word := 0
+	for r := 0; r < rows; r++ {
+		for c := 0; c < cols; c++ {
+			table.SetCell(r, c,
+				tview.NewTableCell(" "+emojis[word]+" "))
+			word = (word + 1) % len(emojis)
+		}
+	}
+}
+
 func main() {
 	app := tview.NewApplication()
 	table := tview.NewTable().
 		SetBorders(false).
 		SetSelectable(true, true).
 		SetFixed(1, 1)
+
+	inputField := tview.NewInputField().
+		SetDoneFunc(func(key tcell.Key) {
+			app.Stop()
+		})
+
+	inputField.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyDown {
+			app.SetFocus(table)
+			table.SetSelectable(true, true)
+		}
+		return event
+	})
+
+	grid := tview.NewGrid().
+		SetRows(1, 1).
+		SetColumns(1, 1).
+		AddItem(inputField, 0, 0, 1, 3, 0, 0, true).
+		AddItem(table, 2, 0, 1, 3, 0, 0, false)
 
 	emojis, _ := getEmojis()
 	justEmojis := []string{}
@@ -56,19 +87,10 @@ func main() {
 			justEmojis = append(justEmojis, emoji.Char)
 		}
 	}
-
 	sort.Sort(sort.Reverse(sort.StringSlice(justEmojis)))
+	drawEmojis(table, justEmojis)
 
-	cols, rows := 10, 200
-	word := 0
-	for r := 0; r < rows; r++ {
-		for c := 0; c < cols; c++ {
-			table.SetCell(r, c,
-				tview.NewTableCell(" "+justEmojis[word]+" "))
-			word = (word + 1) % len(justEmojis)
-		}
-	}
-	table.Select(0, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
+	table.SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
 			table.SetSelectable(true, true)
 			app.Stop()
@@ -77,8 +99,18 @@ func main() {
 		cell := table.GetCell(row, column)
 		clipboard.WriteAll(cell.Text)
 		app.Stop()
+	}).SetSelectable(false, false)
+
+	table.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		row, _ := table.GetSelection()
+		if event.Key() == tcell.KeyUp && row == 0 {
+			app.SetFocus(inputField)
+			table.SetSelectable(false, false)
+		}
+		return event
 	})
-	if err := app.SetRoot(table, true).Run(); err != nil {
+
+	if err := app.SetRoot(grid, true).Run(); err != nil {
 		panic(err)
 	}
 }
